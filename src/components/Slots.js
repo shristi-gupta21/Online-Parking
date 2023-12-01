a36802bd7d8a6f1a4481fa8be9f0b362e8cdfb2e
Shristi Gupta <gshristi40@gmail.com>
2023-12-01 23:58:41 +0530

    trying to fixing a bug

--- src/components/Slots.js
+++ src/components/Slots.js
@@ -23,17 +23,21 @@ const Slots = ({ slotNumber, onShowDetails }) => {
   const dispatch = useDispatch();
   const updateUserData = useSelector(UpdateUsers);
   const [addClick, setAddClick] = useState(false);
+  const [slotNumbersArr, setSlotNumbersArr] = useState([]);
+  const [isPresentSlot, setIsPresentSlot] = useState([]);
   useEffect(() => {
     if (searchedData !== "") {
       showSearchedUser(searchedData);
     } else {
       showSearchedUser(searchedData);
     }
+    userData.map((item) =>
+      setSlotNumbersArr((prev) => [...prev, item.slotNumber])
+    );
   }, [searchedData, updateUserData, userData]);
-
+  console.log(slotNumbersArr);
   const onClickRemoveData = (i) => {
     dispatch(removeUser({ index: i }));
-
     updateUserData !== null && dispatch(updateUser({ index: null }));
     dispatch(addSlot({ slots: slotsData.slots + 1 }));
   };
@@ -59,11 +63,15 @@ const Slots = ({ slotNumber, onShowDetails }) => {
   };
   const onClickAdd = () => {
     setAddClick(true);
-    if(addClick){
+    setIsPresentSlot((prev) => [
+      ...prev,
+      userData.filter((item) => slotNumbersArr.includes(item.slotNumber)),
+    ]);
+    if (addClick) {
       document.querySelector(".add-details").style.display = "block";
     }
   };
-
+  console.log(isPresentSlot);
   return (
     <div>
       <h1 className="text-center py-4 font-semibold w-full text-xl uppercase">
@@ -84,46 +92,54 @@ const Slots = ({ slotNumber, onShowDetails }) => {
                 Add Vehicle
               </button>
             ) : (
-              userData.map((item) =>
-                parseInt(item.slotNumber) === slotNum ? (
-                  <div className="flex w-64 items-start">
-                    <div className="w-4/5">
-                      <p className="text-center">Slot {item.slotNumber}</p>
-                      <p>{item.name}</p>
-                      <p>{item.regNumber}</p>
-                      <p>{item.vehicle}</p>
-                    </div>
-                    {addClick ? (
-                      <div className="flex flex-col items-end w-1/5">
-                        <button
-                          onClick={() => onClickRemoveData(item.slotNumber - 1)}
-                          className=" text-red-500"
-                        >
-                          <RemoveCircleIcon />
-                        </button>
-                        <button className="text-gray-700">
-                          <EditIcon />
-                        </button>
+              userData.map(
+                (item) =>
+                  parseInt(item.slotNumber) === slotNum && (
+                    <div className="flex w-64 items-start">
+                      <div className="w-4/5">
+                        <p className="text-center">Slot {item.slotNumber}</p>
+                        <p>{item.name}</p>
+                        <p>{item.regNumber}</p>
+                        <p>{item.vehicle}</p>
                       </div>
-                    ) : (
-                      <button
-                        className="px-2 py-1 rounded shadow bg-gradient-to-r from-purple-800 to-blue-350 text-white"
-                        onClick={onClickAdd}
-                      >
-                        Add Vehicle
-                      </button>
-                    )}
-                  </div>
-                ) : (
+                      {addClick ? (
+                        <div className="flex flex-col items-end w-1/5">
+                          <button
+                            onClick={() =>
+                              onClickRemoveData(item.slotNumber - 1)
+                            }
+                            className=" text-red-500"
+                          >
+                            <RemoveCircleIcon />
+                          </button>
+                          <button className="text-gray-700">
+                            <EditIcon />
+                          </button>
+                        </div>
+                      ) : (
+                        <button
+                          className="px-2 py-1 rounded shadow bg-gradient-to-r from-purple-800 to-blue-350 text-white"
+                          onClick={onClickAdd}
+                        >
+                          Add Vehicle
+                        </button>
+                      )}
+                    </div>
+                  )
+              )
+            )}
+            {userData.length > 0 &&
+              userData.map((item) =>
+                isPresentSlot.map(slot => (
+                  slot === item.slotNumber ? "work":
                   <button
                     className="px-2 rounded py-1 shadow bg-gradient-to-r from-purple-800 to-blue-350 text-white"
                     onClick={onClickAdd}
                   >
                     2.Add Vehicle
                   </button>
-                )
-              )
-            )}
+                )) 
+              )}
           </div>
         ))}
       </div>
